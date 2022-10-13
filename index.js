78179cea1f07fe1cfaba5ad1efd5e00e35d11b19
likewu <likewu@126.com>
2022-10-13 05:42:12 +0000

    aa

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const http = require('http');
 const setup = require('proxy');
+const finalhandler = require('finalhandler')
 const morgan = require("morgan");
 
-var server = setup(http.createServer());
+var logger = morgan('combined')
+
+let cs = http.createServer(function (req, res) {
+  var done = finalhandler(req, res)
+  logger(req, res, function (err) {
+    if (err) return done(err)
+ 
+    // respond to request
+    res.setHeader('content-type', 'text/plain')
+    res.end('hello, world!')
+  })
+})
+
+var server = setup(cs);
 server.listen(3128, function () {
   var port = server.address().port;
   console.log('HTTP(s) proxy server listening on port %d', port);
